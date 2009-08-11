2334086856e365e6c5d05d44d6f3433151ab1a47
Jessica Tsuji <jessicatsuji@jessica-tsujis-macbook-pro.local>
2009-08-10 19:50:50 -0700

    Added Basic Zend Framework

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+	class IndexController extends Zend_Controller_Action
+	{
+		public function init() {
+			//$this->session_alert = new Zend_Session_Namespace('');
+			//$this->Model = new Model();
+			//$this->_helper->layout->setLayout('');
+		}
+		
+		public function indexAction()
+		{
+
+		}
+		
+	}
+	
+?>
